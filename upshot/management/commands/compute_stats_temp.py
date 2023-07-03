8bf9e85b491ed20cfa8ffa2f79f0fe5abf4cb56c
rolanveroncruz <rolanveroncruz@gmail.com>
2023-07-03 14:07:47 +0800

    moved some code to a temp file

---
+++ upshot/management/commands/compute_stats_temp.py
@@ -0,0 +1,63 @@
+
+# 9
+# 10
+# 11
+# 12
+# ave_days_to_closed_capture_all = ave_days_to_complete_capture(user, from_datetime, to_datetime, FBType.ALL)
+# # 13
+# ave_days_to_close_capture_positive = ave_days_to_complete_capture(user, from_datetime, to_datetime, FBType.POS)
+# # 14
+# ave_days_to_close_capture_corrective = ave_days_to_complete_capture(user, from_datetime, to_datetime, FBType.CORR)
+# # 15
+# ave_days_to_start_re_all = ave_days_to_start_recording_entries(user, from_datetime, to_datetime, FBType.ALL)
+# # 16
+# ave_days_to_start_re_positive = ave_days_to_start_recording_entries(user, from_datetime, to_datetime, FBType.POS)
+# # 17
+# ave_days_to_start_re_corrective = ave_days_to_start_recording_entries(user, from_datetime, to_datetime, FBType.CORR)
+# # 18
+# ave_days_to_complete_re_all = ave_days_to_complete_record_entries(user, from_datetime, to_datetime, FBType.ALL)
+# # 19
+# ave_days_to_complete_re_positive = ave_days_to_complete_record_entries(user, from_datetime, to_datetime, FBType.POS)
+# # 20
+# ave_days_to_complete_re_corrective = ave_days_to_complete_record_entries(user, from_datetime, to_datetime, FBType.CORR)
+# # 21
+# # 22
+# # 23
+# # 24
+# ave_days_to_complete_rs_all = ave_days_to_complete_review_and_submit(user, from_datetime, to_datetime, FBType.ALL)
+# # 25
+# ave_days_to_complete_rs_positive = ave_days_to_complete_review_and_submit(user, from_datetime, to_datetime, FBType.POS)
+# # 26
+# ave_days_to_complete_rs_corrective = ave_days_to_complete_review_and_submit(user, from_datetime, to_datetime, FBType.CORR)
+# # 27
+# ave_days_to_complete_discuss_all = ave_days_to_complete_discussion(user, from_datetime, to_datetime, FBType.ALL)
+# # 28
+# ave_days_to_complete_discuss_positive = ave_days_to_complete_discussion(user, from_datetime, to_datetime, FBType.POS)
+# # 29
+# ave_days_to_complete_discuss_corrective = ave_days_to_complete_discussion(user, from_datetime, to_datetime, FBType.CORR)
+# # 37
+# ave_days_to_close_reflection_all = ave_days_to_complete_checklist(user, from_datetime, to_datetime, FBType.ALL)
+# # 38
+# ave_days_to_close_reflection_positive = ave_days_to_complete_checklist(user, from_datetime, to_datetime, FBType.POS)
+# # 39
+# ave_days_to_close_reflection_corrective = ave_days_to_complete_checklist(user, from_datetime, to_datetime, FBType.CORR)
+
+
+# mb_stats.ave_days_to_close_capture_all = ave_days_to_closed_capture_all
+# mb_stats.ave_days_to_close_capture_positive = ave_days_to_close_capture_positive
+# mb_stats.ave_days_to_close_capture_corrective = ave_days_to_close_capture_corrective
+# mb_stats.ave_days_to_start_record_entries_all = ave_days_to_start_re_all
+# mb_stats.ave_days_to_start_record_entries_positive = ave_days_to_start_re_positive
+# mb_stats.ave_days_to_start_record_entries_corrective = ave_days_to_start_re_corrective
+# mb_stats.ave_days_to_complete_record_entries_all = ave_days_to_complete_re_all
+# mb_stats.ave_days_to_complete_record_entries_positive = ave_days_to_complete_re_positive
+# mb_stats.ave_days_to_complete_record_entries_corrective = ave_days_to_complete_re_corrective
+# mb_stats.ave_days_to_complete_rs_all = ave_days_to_complete_rs_all
+# mb_stats.ave_days_to_complete_rs_positive = ave_days_to_complete_rs_positive
+# mb_stats.ave_days_to_complete_rs_corrective = ave_days_to_complete_rs_corrective
+# mb_stats.ave_days_to_complete_discuss_all = ave_days_to_complete_discuss_all
+# mb_stats.ave_days_to_complete_discuss_positive = ave_days_to_complete_discuss_positive
+# mb_stats.ave_days_to_complete_discuss_corrective = ave_days_to_complete_discuss_corrective
+# mb_stats.ave_days_to_close_reflection_all = ave_days_to_close_reflection_all
+# mb_stats.ave_days_to_close_reflection_positive = ave_days_to_close_reflection_positive
+# mb_stats.ave_days_to_close_reflection_corrective = ave_days_to_close_reflection_corrective
